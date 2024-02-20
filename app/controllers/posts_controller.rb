8899180606ecf07652eab6a5a348430b7a3058a0
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-02-20 11:02:50 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,6 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
-
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
